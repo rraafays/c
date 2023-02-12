8c67c94a0e54183616ef8fd23f663dc651ab784c
raf <rraf@tuta.io>
2023-02-12 14:01:19 +0000

    update: completed ex3

---
+++ 3.formatted-printing/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // include standard input output header file
+
+int main(int argc, char *argv[]) // proper main block
+{
+  int age = 20; // age integer
+  int height = 183;  // height integer
+
+  printf("I am %d years old at the time of writing this code.\n", age); // print age
+  printf("I am %d centimetres tall at the time of writing this code.\n", height); // print height
+
+  return 0; // exit code 0
+}
